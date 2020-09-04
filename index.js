8dfae7edca6e150448f1dfd2246b9d8e35954a79
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-09-04 20:40:52 +0530

    axios boilerplate

---
+++ index.js
@@ -0,0 +1,36 @@
+const Axios = require('axios');
+
+let token = "";
+
+const config = {
+    headers: {     
+    "Authorization": `Bearer ${token}`
+}
+};
+
+// 'application/x-www-form-urlencoded'
+
+// const bodyParameters = {
+//    address: "",
+//    city:"",
+//    email:"",
+//    firstname:"",
+//    lastname:"",
+//    mobile:"",
+//    province:""
+// };
+let path = `y97u3r7rjb`
+
+Axios.get( 
+  `enter your url here`,
+  config
+).then((data)=>{
+  let details =data;
+console.log(details);
+}).catch(err =>{
+    console.log(err);
+});
+
+
+
+// https://github.com/axios/axios/issues/362
